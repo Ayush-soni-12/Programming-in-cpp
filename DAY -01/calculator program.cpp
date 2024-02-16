58d33ad04d4dba7b5f3f44f3a1c712fc70e972ea
Ayush-soni-12 <151499723+Ayush-soni-12@users.noreply.github.com>
2024-02-16 20:55:43 +0530

    Update calculator program.cpp

--- DAY -01/calculator program.cpp
+++ DAY -01/calculator program.cpp
@@ -1,1 +1,13 @@
-sk
+#include <iostream>
+using namespace std;
+int main()
+{
+    float a,b;
+    cout<<"enter a and b is"<<endl;
+    cin>>a>>b;
+    cout<<"Addition of a and b is "<<a+b<<endl;
+    cout<<"Subtraction of a and b is "<<a-b<<endl;
+    cout<<"multiplication of a and b is"<<a*b<<endl;
+    cout<<"division of a and b is"<<a/b<<endl;
+    return 0;
+}
