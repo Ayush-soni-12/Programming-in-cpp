595e0bd6080d1a36c449e4fcb4f6d83ee6d5de1d
Ayush-soni-12 <151499723+Ayush-soni-12@users.noreply.github.com>
2024-02-20 02:15:57 +0530

    Update check even and odd.cpp

--- DAY -02/check even and odd.cpp
+++ DAY -02/check even and odd.cpp
@@ -1,1 +1,16 @@
-i
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout<<"Enter number is :"<<endl;
+    cin>>n;
+    if(n%2==0){
+        cout<<"Number is even"<<endl;
+        
+        
+    }
+    else{
+        cout<<"Number is odd";
+}
+    return 0;
+}i
