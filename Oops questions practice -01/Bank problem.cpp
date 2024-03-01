7011e0f490bf007f8a71062c59e5322927d57a89
Ayush-soni-12 <151499723+Ayush-soni-12@users.noreply.github.com>
2024-03-02 01:42:37 +0530

    Update Oops questions practice -01

---
+++ Oops questions practice -01/Bank problem.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+class BankAccount
+{
+    private :
+    
+    string bankname;
+    int accountNumber;
+    double balance;
+    public :
+    
+    double depositamount;
+    double currentbalance;
+    double withdrawalamount;
+    
+    BankAccount(string a,double Ac,double b){
+        
+        bankname =a;
+        accountNumber=Ac;
+        balance=b;
+        
+        cout<<"Bank name = "<<bankname<<endl;
+        cout<<"Account Number= "<<accountNumber<<endl;
+        cout<<"Balance ="<<balance<<endl;
+        
+    }
+    void deposit();
+    void withdraw(); 
+    
+};
+void BankAccount :: deposit(){
+   
+    cout<<"Deposit amount ="<<endl;
+    cin>>depositamount;
+    cout<<"Deposit successfull "<<endl;
+    currentbalance=depositamount + balance;
+    cout<<"Current Balance = "<<currentbalance<<endl;
+}
+void BankAccount :: withdraw(){
+    cout<<" withdrawal amount = "<<endl;
+    cin>>withdrawalamount;
+    if (withdrawalamount<=currentbalance){
+    cout<<" withdrawal successfull "<<endl;
+    currentbalance = currentbalance-withdrawalamount;
+    cout<<" Current Balance = "<<currentbalance<<endl;
+    }
+    else{
+        cout<<"withdrawal unsuccessful. Insufficient balance "<<endl;
+    }
+}
+int main()
+{
+    BankAccount BA("SBI",13435663,1000.00);
+    
+    
+    BA.deposit();
+    BA.withdraw();
+   
+    return 0;
+}
