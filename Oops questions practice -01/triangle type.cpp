0f7758afa012e1c7214e010149c1897b97354fba
Ayush-soni-12 <151499723+Ayush-soni-12@users.noreply.github.com>
2024-03-03 00:48:53 +0530

    Update Oops questions practice -01

---
+++ Oops questions practice -01/triangle type.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+class Triangle
+{
+    private :
+    
+    int L1,L2,L3;
+    
+    public :
+   
+    void setlength(){
+        cout<<"Enter length of triangle = "<<endl;
+        cin>>L1>>L2>>L3;
+    chktriangle();
+    }
+    void chktriangle();
+    void triangle_type();
+};
+void Triangle :: chktriangle(){
+    if (L1+L2>L3 && L2+L3>L1 && L1+L3>L2){
+        cout <<"Trinagle formation is possible : "<<endl;
+        triangle_type();
+    }
+    else {
+        cout<<" Trinagle formation are not possible : "<<endl;
+    }
+    
+}
+void Triangle :: triangle_type(){
+    if (L1==L2 && L2==L3 && L3==L1){
+        cout<<"Equilateral triangle: "<<endl;
+    }
+    else if (L1==L2 || L2==L3 ||L3==L1){
+        cout <<" Isoclecs triangle: "<<endl;
+        
+    }
+    else {
+        cout<<" scalene triangle: "<<endl;
+    }
+}
+int main()
+{
+    Triangle T;
+    T.setlength();
+   // T.chktriangle();
+  //  T.triangle_type();
+    return 0;
+}
